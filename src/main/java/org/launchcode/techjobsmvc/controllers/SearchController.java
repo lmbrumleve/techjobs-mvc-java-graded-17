0f3046c8af125f72a04ee7db0388de382ad7cfaf
lmbrumleve <142369731+lmbrumleve@users.noreply.github.com>
2024-01-20 17:24:57 -0600

    pt 1 bonus done - keep search field selected

--- src/main/java/org/launchcode/techjobsmvc/controllers/SearchController.java
+++ src/main/java/org/launchcode/techjobsmvc/controllers/SearchController.java
@@ -28,9 +28,27 @@ public class SearchController {
         return "search";
     }
 
+    @PostMapping(value = "")
+    public String displaySearch(Model model,
+                                       @RequestParam String searchType,
+                                       @RequestParam String searchTerm) {
+        ArrayList<Job> jobs = new ArrayList<Job>();
+        if (Objects.equals(searchTerm, "all") || searchTerm.isEmpty()) {
+            jobs = JobData.findAll();
+        } else {
+            jobs = JobData.findByColumnAndValue(searchType, searchTerm);
+        }
+        model.addAttribute("jobs", jobs);
+        model.addAttribute("columns", columnChoices);
+        model.addAttribute("searchType", searchType);
+        return "search";
+    }
+
     // TODO #3 - Create a handler to process a search request and render the updated search view.
     @PostMapping(value = "results")
-    public String displaySearchResults(Model model, @RequestParam String searchType, @RequestParam String searchTerm) {
+    public String displaySearchResults(Model model,
+                                       @RequestParam String searchType,
+                                       @RequestParam String searchTerm) {
         ArrayList<Job> jobs = new ArrayList<Job>();
         if (Objects.equals(searchTerm, "all") || searchTerm.isEmpty()) {
            jobs = JobData.findAll();
@@ -39,6 +57,7 @@ public class SearchController {
         }
         model.addAttribute("jobs", jobs);
         model.addAttribute("columns", columnChoices);
+        model.addAttribute("searchType", searchType);
         return "search";
     }
 }
